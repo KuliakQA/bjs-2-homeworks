87bb701cdf8c73e8b48b507ca7bc09e9375e7e96
KuliakQA <82388284+KuliakQA@users.noreply.github.com>
2022-03-15 18:50:42 +0100

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,22 @@
 "use strict"
 
+
 function solveEquation(a, b, c) {
-	 let arr = [];
-	 let d = b**2 - 4*a*c;
-	 let x, x1, x2;
-  
-  
-	 if (d == 0){
-		x = -b / (2*a);
-		arr.push(x);
-	 }
-	 else (d > 0){
-		x1 = (-b + Math.aqrt(d)) / (2*a);
-		x2 = (-b - Math.aqrt(d)) / (2*a);
-		arr.push(x1, x2);
-	 }
-  return arr;
-  }
+    let arr = [];
+    let d = b*b - 4*a*c;
+    let x1, x2;
 
-
+   if (d > 0) {
+       x1 = (-b + Math.sqrt(d)) / (2 * a);
+       x2 = (-b - Math.sqrt(d)) / (2 * a);
+       arr.push(x1, x2);
+   }
+   if (d === 0) {
+       x1 = -b / (2 * a);
+       arr.push(x1);
+   }
+   else if (d < 0) {
+       console.log("No real solutions");
+   }
+ return arr;
+ }
