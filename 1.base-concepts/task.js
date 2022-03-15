da94c7412f3adbb71d728143c1843fb84e9bcd76
KuliakQA <82388284+KuliakQA@users.noreply.github.com>
2022-03-15 16:30:07 +0100

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,24 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+	 let arr = [];
+	 let d = b**2 - 4*a*c;
+	 let x, x1, x2;
+  
+  
+	 if (d === 0){
+		x = -b / (2*a);
+		arr.push(x);
+	 }
+	 else if(d > 0){
+		x1 = (-b + Math.aqrt(d)) / (2*a);
+		x2 = (-b - Math.aqrt(d)) / (2*a);
+		arr.push(x1, x2);
+	 }
+  return arr;
+  }
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
