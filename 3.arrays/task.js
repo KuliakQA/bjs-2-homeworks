b594ffd1e1dfc60f830e473081e3daacf752b7bf
KuliakQA <82388284+KuliakQA@users.noreply.github.com>
2022-04-03 13:19:50 +0200

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
-function compareArrays(arr1, arr2) {
-  let result;
+//task 1
 
-  // Ваш код
+let arr1 = [8, 9];
+let arr2 = [6];
+let compareArrays = (a1, a2) =>
+  a1.length === a2.length &&
+  a1.every((element, index) => element === a2[index]);
+console.log(compareArrays(arr1, arr2));
 
-  return result; // boolean
-}
+//task2
 
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+let advancedFilter = (arr) => arr
+  .filter((item) => item >= 0 && item % 3 == 0)
+  .map((item) => item * 10);
+console.log(advancedFilter([4, 3, 5]));
