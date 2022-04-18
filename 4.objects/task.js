d1fa5b20db5edf5e35efa8784e0b25d76955c992
KuliakQA <82388284+KuliakQA@users.noreply.github.com>
2022-04-18 11:56:10 +0200

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -19,18 +19,17 @@ Student.prototype.addMark = function(mark) {
 }
 
 //метод addMarks
-Student.prototype.addMarks = function(...markAll) {
+Student.prototype.addMarks = function(...mark) {
   if (this.marks === undefined) {
-    this.marks = markAll;
+    this.marks = [...mark];
   } else {
-    for (let i = 0; i < markAll.length; i++)
-      this.marks.push(markAll[i]);
+    this.marks.push(...mark);
   }
 }
 
 //метод getAverage
 Student.prototype.getAverage = function() {
-  this.average = this.marks.reduce((acc, item, idx, arr) => {
+  return this.marks.reduce((acc, item, idx, arr) => {
     if (idx === arr.length - 1) {
       return (acc + item) / arr.length;
     } else {
@@ -48,15 +47,10 @@ Student.prototype.exclude = function(reason) {
 
 let student1 = new Student("Tony", "male", 37);
 student1.setSubject("Algebra");
-student1.addMark(5);
-student1.addMark(4);
-student1.addMark(5);
-console.log(student1.getAverage()); //4.6666
-console.log(student1);
-// {age: 37, gender: "male", marks: [5, 4, 5], name: "Tony", subject: "Algebra"}
+
 let student2 = new Student("Buzz", "female", 35);
 student2.setSubject("Geometry");
-student2.addMarks(2, 3, 4, 4, 3);
-student2.exclude('low grades')
-console.log(student2)
-// {name: "Buzz", gender: "female", age: 35, excluded: "low grades"}
+student2.addMark(7);
+student2.addMarks(5, 4, 5);
+console.log(student2.getAverage());
+console.log(student2);
